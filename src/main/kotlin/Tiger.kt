5a4869f72c4f36ee5fac7352af210775eef33b14
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2025-03-02 10:53:24 -0300

    feat: heranca em kotlin

---
+++ src/main/kotlin/Tiger.kt
@@ -0,0 +1,12 @@
+open class Tiger(val origin: String) {
+    fun sayHello() {
+        println("A tiger from $origin says: grrrrh!")
+    }
+}
+
+class SiberianTiger : Tiger("Siberia")
+
+fun main() {
+    val tiger: Tiger = SiberianTiger()
+    tiger.sayHello()
+}
